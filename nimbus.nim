783fe71e1ad092f0a8dfa6c9089904ac72cc6193
Oleh Prypin <blaxpirit@gmail.com>
2015-02-22 23:46:49 +0200

    Allowed input to end with an expression

--- nimbus.nim
+++ nimbus.nim
@@ -142,7 +142,11 @@ while true:
             var outHandle = open(filename, fmWrite)
             var result: string
 
-            outHandle.writeln(replace(join(ircmsg[4 .. ircmsg.high], " "), ";", "\n"))
+            outHandle.writeln("proc eval(): auto =")
+            for line in split(join(ircmsg[4 .. ircmsg.high], " "), ";"):
+                outHandle.writeln(" " & line)
+            outHandle.writeln("when compiles(echo eval()): echo eval()")
+            outHandle.writeln("else: eval()")
             close(outHandle)
 
             var resultInitial = execCmdEx("nim compile --stackTrace:off --lineTrace:off --threads:off --checks:off --fieldChecks:off --rangeChecks:on --boundChecks:on --overflowChecks:on --assertions:on --floatChecks:off --nanChecks:on --infChecks:off --opt:none --warnings:off --hints:off --threadanalysis:off --verbosity:0 --cc:ucc " & filename)
